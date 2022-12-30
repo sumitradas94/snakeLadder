1789d3ff179d227196aad948462f026ac2d22c01
sumitradas94 <sumitradas94@gmail.com>
2022-12-30 23:17:00 +0530

    random die between 1 to 6

---
+++ Randomdie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Randomdie
+    {
+        public static void Startrandom()
+        {
+            int startPosition = 0;
+            Console.WriteLine("Snake and Ladder game played with single player at start position : " + startPosition);
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("The Player rolls the die to get a number between 1 to 6 is : " + die);
+        }
+    }
+}
