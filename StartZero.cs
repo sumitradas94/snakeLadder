b49e7d246ef7ae5c9b1227cde9365d4872dc58ef
sumitradas94 <sumitradas94@gmail.com>
2022-12-30 22:39:57 +0530

    startingzero

---
+++ StartZero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class StartZero
+    {
+        public static void Zero()
+        {
+            int startposition = 0;
+            Console.WriteLine("Snake and Ladder game played with single player at start position : \"+startPosition");
+        }
+    }
+}
