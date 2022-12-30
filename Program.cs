07e4bce0dcb924f1f7ae4100c2ba3b654c445754
sumitradas94 <sumitradas94@gmail.com>
2022-12-30 22:36:26 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using SnakeLadder;
+
+namespace snakeLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snack and ladder Game\n");
+            StartZero.Zero();
+
+        }
+    }
+}
