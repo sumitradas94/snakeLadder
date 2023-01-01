f3a8aa8c7e47f08af2dca8d51cf2feee1af97f11
sumitradas94 <sumitradas94@gmail.com>
2023-01-01 21:40:22 +0530

    optionplay

---
+++ OptionPlay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class OptionPlay
+    {
+        public static void start()
+        {
+            int position = 0;
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            int prop = random.Next(3);
+            const int NoPlay = 0;
+            const int ladder = 1;
+            const int snake = 2;
+            switch (prop)
+            {
+                case NoPlay:
+                    break;
+                case ladder:
+                    position = die;
+                    break;
+                case snake:
+                    position = position - die;
+                    break;
+            }
+            Console.WriteLine("die::{0} \t option::{1} \t position::{2}", die, prop, position);
+        }
+    }
+}
